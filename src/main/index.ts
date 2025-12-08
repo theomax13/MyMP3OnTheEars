746805e66b92d3216a415c85e56d5f43d86187bc
MAXIME Théo <theomax13@hotmail.fr>
2025-12-08 19:27:26 +0100

    pinia

--- src/main/index.ts
+++ src/main/index.ts
@@ -12,6 +12,7 @@ function createWindow(): void {
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
+      contextIsolation: true,
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
     }
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,7 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
 
-createApp(App).use(PrimeVue).mount('#app')
+createApp(App).use(PrimeVue, createPinia).mount('#app')
