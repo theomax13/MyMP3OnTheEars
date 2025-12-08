96125b2021c2205c7dcb0d0d36116c7cf3ec204a
MAXIME Théo <theomax13@hotmail.fr>
2025-12-08 16:37:23 +0100

    PrimeVue

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,5 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue).mount('#app')
